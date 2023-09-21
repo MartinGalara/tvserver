9ebb64d36423b19112161722921c6f51b5504b89
Martin Galara <mgalara@gmail.com>
2023-09-21 11:21:56 -0300

    asd

--- src/routes/controllers/devices.js
+++ src/routes/controllers/devices.js
@@ -6,6 +6,66 @@ const axios = require('axios')
 
 require('dotenv').config();
 
+router.get('/', async (req, res) => {
+
+  const accessToken = process.env.TEAMVIEWER_BEARER_TOKEN; // Obtén el token de acceso de tus variables de entorno
+
+    // Configura las opciones de la solicitud GET a la API de TeamViewer
+    const options = {
+      method: 'get',
+      url: 'https://webapi.teamviewer.com/api/v1/devices?full_list=true', // Incluye '?full_list=true' para obtener la lista completa
+      headers: {
+        'Authorization': `Bearer ${accessToken}`,
+        'Content-Type': 'application/json'
+      }
+    };
+
+    // Realiza la solicitud GET
+    const response = await axios(options);
+
+      // Filtra los dispositivos cuyo alias contiene 6 símbolos "|"
+      const devices = response.data.devices.filter(device => {
+        const alias = device.alias || '';
+        const pipeCount = (alias.match(/\|/g) || []).length;
+        return pipeCount === 6;
+      });
+
+    const formattedDevices = [];
+
+      devices.forEach(device => {
+        const aliasParts = device.alias.split('|');
+        const formattedDevice = {
+          razonSocial: aliasParts[0],
+          bandera: setBandera(aliasParts[1]),
+          identificador: aliasParts[2],
+          ciudad: aliasParts[3],
+          area: aliasParts[4],
+          prefijo: aliasParts[5],
+          extras: aliasParts.slice(6).join('|'),
+          teamviewer_id: device.teamviewer_id.toString(),
+          clientId:"",
+          alias:""
+        };
+
+        formattedDevice.clientId = formattedDevice.bandera.substring(0, 2) + formattedDevice.identificador;
+        formattedDevice.alias = formattedDevice.area + formattedDevice.teamviewer_id.toString()
+
+        formattedDevices.push(formattedDevice);
+      });
+
+       for (const formattedDevice of formattedDevices) {
+        const endpointUrl = `${process.env.SIGES_SERVER}/pcs?from=tv`;
+
+        // Realiza una solicitud Axios put para enviar formattedDevice al endpoint
+        await axios.put(endpointUrl, formattedDevice);
+
+      }
+
+      res.status(200).json({ message: 'Datos procesados y enviados al endpoint' });
+
+      
+});
+
 // Ruta para obtener todas las computadoras o filtrar por clientId y zona
 router.post('/', async (req, res) => {
 
@@ -121,7 +181,13 @@ const amTeamViewer = async (devices) => {
       }
 
       // Realizar solicitudes axios para actualizar dispositivos en devicesToUpdate
-    for (const device of devicesToUpdate) {
+      for (const device of devicesToUpdate) {
+        // Verificar si tvalias contiene 6 símbolos "|"
+        if (device.tvalias.split('|').length === 7) {
+          console.log(`El dispositivo con tvalias '${device.tvalias}' tiene 6 símbolos "|". Se omitirá.`);
+          continue; // Salta al siguiente dispositivo
+        }
+      
         const updateDeviceOptions = {
           method: 'put',
           url: `https://webapi.teamviewer.com/api/v1/devices/${device.device_id}`,
@@ -133,18 +199,43 @@ const amTeamViewer = async (devices) => {
             alias: device.tvalias,
           }
         };
-  
+      
         try {
           await axios(updateDeviceOptions);
         } catch (updateError) {
-          console.error('Error al actualizar el dispositivo:',updateError);
+          console.error('Error al actualizar el dispositivo:', updateError);
         }
       }
-  
-  
+
     } catch (error) {
       console.error('Error al hacer la solicitud a TeamViewer:', error);
     }
+  }
+
+  const setBandera = (bandera) => {
+
+    switch (bandera.toUpperCase()) {
+      case "YPF": 
+          return "YPF"
+      case "SHE": 
+          return "SHELL"
+      case "AXI": 
+          return "AXION"
+      case "PUM": 
+          return "PUMA"
+      case "GUL": 
+          return "GULF"
+      case "REF": 
+          return "REFINOR"
+      case "BLA": 
+          return "BLANCA"
+      case "OTR": 
+          return "OTRO"
+     
+      default:
+          return "OTRO"
+     }
+
   }
   
 module.exports = router;
